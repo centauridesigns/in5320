b171de46087c1623b969bdde361d43b09bb0c19f
murvold <madsmurvold@gmail.com>
2023-10-13 13:35:51 +0200

    Started work on manage.js
    Added support for base api

--- dhis-base/dhis-app/src/Manage.js
+++ dhis-base/dhis-app/src/Manage.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { useDataQuery } from '@dhis2/app-runtime'
+import { Menu, MenuItem, Table, TableHead, TableRow, TableCell } from "@dhis2/ui";
+
+//the apirequest
+const request = {
+  request0: {
+      resource: "/dataSets/ULowA8V3ucd"
+  }
+}
 
 export function Manage() {
+  const { loading, error, data } = useDataQuery(request)
+
+  if (error) {
+    return <span>ERROR: {error.message}</span>
+  }
+
+  if (loading) {
+    return <span>Loading...</span>
+  }
+
+  if (data){
+    console.log("repons: " + JSON.stringify(data))
+    console.log("\ndatasets: " + JSON.stringify(data["dataSetElements"]))
+  }
+
   return <h1>Manage</h1>;
 }
