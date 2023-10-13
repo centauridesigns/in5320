e4ed674417e9a5fba352615183d9adb9f17fbc73
centauridesigns <souhailz@ulrik.uio.no>
2023-10-13 19:48:04 +0200

    API response returns commodities correctly
    API response now returns commodities correctly. An additional API request also occurs for dataValueSets, but we do nothing with it for the time being. In the future, this will be used to find the number/amount of a given item.

--- dhis-base/dhis-app/src/Manage.js
+++ dhis-base/dhis-app/src/Manage.js
@@ -1,16 +1,35 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useDataQuery } from '@dhis2/app-runtime'
 import { Menu, MenuItem, Table, TableHead, TableRow, TableCell } from "@dhis2/ui";
 
-//the apirequest
-const request = {
-  request0: {
-      resource: "/dataSets/ULowA8V3ucd"
+//API request
+const query = {
+  dataSets: { // Enter the datasets
+    resource: "/dataSets/ULowA8V3ucd?fields=dataSetElements[dataElement[name, id, *]]",  // URL resource, grabs name and ID of commodity
+    params: { // Related parameters to send (none)
+      fields: [ // Specific fields to return
+        'name',
+        'id',
+      ],
+    },
+  },
+  // Potential dataValueSets call, alternatively make other requests here
+  dataValueSets: {
+    resource: "/dataValueSets/",
+    params: { // Related parameters to sen
+      orgUnit: 'KiheEgvUZ0i',
+      period: '202310',
+      dataSet: 'ULowA8V3ucd',
+      fields: [
+        'dataElement', // Data element so we can match items from dataSets with items from dataValueSets
+        'dataValues[value]', // Extract value (number of items), unknown if this is the actual number?
+      ]
+    },
   }
 }
 
 export function Manage() {
-  const { loading, error, data } = useDataQuery(request)
+  const { loading, error, data } = useDataQuery(query)
 
   if (error) {
     return <span>ERROR: {error.message}</span>
@@ -20,9 +39,18 @@ export function Manage() {
     return <span>Loading...</span>
   }
 
-  if (data){
-    console.log("repons: " + JSON.stringify(data))
-    console.log("\ndatasets: " + JSON.stringify(data["dataSetElements"]))
+  if (data) {
+    //console.log("RESPONSE: " + JSON.stringify(data))
+    //console.log("\ndatasets: " + JSON.stringify(data["dataSetElements"]))
+
+    const commodities = data.dataSets.dataSetElements.map(dataElement => ({
+      id: dataElement.dataElement.id,
+      name: dataElement.dataElement.name
+    }));
+
+    // TODO: Find, extract, match, and map values before displaying them.
+
+    console.log(commodities)
   }
 
   return <h1>Manage</h1>;
