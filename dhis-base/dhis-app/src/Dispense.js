11e6664ee83e348c4f7e3272ff189add23e083a3
murvold <madsmurvold@gmail.com>
2023-10-16 21:45:18 +0200

    Added functionality of new entries in Dispense

--- dhis-base/dhis-app/src/Dispense.js
+++ dhis-base/dhis-app/src/Dispense.js
@@ -6,8 +6,35 @@ import "./Dispense.css";
 
 export function Dispense(props) {
     const { mergedData } = props;
-    //console.log("Merged Data in Dispense:", mergedData);
-    /*
+    const [entries, setEntries] = useState([]);
+
+    const handleAddEntry = () => {
+        setEntries(prevEntries => [...prevEntries, { id: Date.now(), amount: 0, commodity: "" }]);
+    };
+
+    const handleRemoveEntry = (id) => {
+        setEntries(prevEntries => prevEntries.filter(entry => entry.id !== id));
+    };
+
+    return(
+        <div>
+            <h1>Dispense</h1>
+            <div>
+                {entries.map(entry => (
+                    <NewEntry
+                        key={entry.id}
+                        mergedData={mergedData}
+                        onRemove={() => handleRemoveEntry(entry.id)}
+                    />
+                ))}
+                <Button className="button" type="button" onClick={handleAddEntry}>Add</Button>
+            </div>
+        </div>
+    )
+}
+
+
+function NewEntry({mergedData, onRemove}){
     const [amount, setAmount] = useState(0);
     const [selectedCommodity, setSelectedCommodity] = useState("");
 
@@ -23,7 +50,6 @@ export function Dispense(props) {
 
     return (
         <div>
-            <h1>Dispense</h1>
             <div className="container">
                 <div className="leftSide">
                     <SingleSelect className="select" placeholder="Choose a commodity" onChange={handleSelectChange} selected={selectedCommodity.selected}>
@@ -36,51 +62,7 @@ export function Dispense(props) {
                     <Input className="numberInput" placeholder="Write amount of packages" type="number" min="0" max="1000" onChange={handleAmountChange}></Input>
                 </div>
                 <div>
-                    <Button className="button" type="button" onClick="">Add</Button>
-                </div>
-            </div>
-        </div>
-    );*/
-    return(
-        <div>
-            <div>
-                <NewEntry mergedData={mergedData}/>
-            </div>
-            <div>
-                <Button className="button" type="button" onClick="">Add</Button>
-            </div>
-        </div>
-    )
-}
-
-
-function NewEntry({mergedData}){
-    const [amount, setAmount] = useState(0);
-    const [selectedCommodity, setSelectedCommodity] = useState("");
-
-    const handleSelectChange = (value) => {
-        setSelectedCommodity(value);
-        console.log("selected value: ", value.selected);
-    };
-
-    const handleAmountChange = (amount) => {
-        setAmount(amount);
-        console.log("selected amount: ", amount.value);
-    };
-
-    return (
-        <div>
-            <h1>Dispense</h1>
-            <div className="container">
-                <div className="leftSide">
-                    <SingleSelect className="select" placeholder="Choose a commodity" onChange={handleSelectChange} selected={selectedCommodity.selected}>
-                        {mergedData.map((commodity) => 
-                            <SingleSelectOption key={commodity.id} label={`${commodity.name} (${commodity.value})`} value={commodity.id} />
-                        )
-                    }</SingleSelect>
-                </div>
-                <div className="rightSide">
-                    <Input className="numberInput" placeholder="Write amount of packages" type="number" min="0" max="1000" onChange={handleAmountChange}></Input>
+                    <Button className="remove-button" type="button" onClick={onRemove}>X</Button>
                 </div>
             </div>
         </div>
