b26a5386f1004ffe113d0bb03c166b01118d1d04
centauridesigns <souhailz@ulrik.uio.no>
2023-11-13 15:33:21 +0100

    Add date formatting to transactions

--- dhis-base/dhis-app/src/Dashboard.js
+++ dhis-base/dhis-app/src/Dashboard.js
@@ -5,6 +5,27 @@ import { IconCross24, IconAdd24, IconFaceAdd24, IconCheckmark24, IconCheckmarkCi
 import { getPersonnel, getTransactions, postNewPersonnel } from "./api.js";
 import "./Dashboard.css";
 
+function formatDate(dateString) {
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const suffixes = ["th", "st", "nd", "rd"];
+
+  const date = new Date(dateString);
+  const day = date.getDate();
+  const monthIndex = date.getMonth();
+  const year = date.getFullYear();
+  const hours = date.getHours();
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+
+  const suffix = (day % 10 === 1 && day !== 11) ? suffixes[1] :
+    (day % 10 === 2 && day !== 12) ? suffixes[2] :
+      (day % 10 === 3 && day !== 13) ? suffixes[3] : suffixes[0];
+
+  const formattedDate = `${day}${suffix} of ${months[monthIndex]} ${year} (${hours % 12 || 12}:${minutes}:${seconds} ${ampm})`;
+  return formattedDate;
+}
+
 export function Dashboard() {
   const { loading, error, data } = useDataQuery(getTransactions());
 
@@ -20,15 +41,15 @@ export function Dashboard() {
         <div className="table" key={transaction.id}>
           <Table>
             <TableHead>
-              <TableRow>
-                <TableCell className="tableCell">{`${transaction.action} - ${transaction.time}`}</TableCell>
+              <TableRow className="table-header">
+                <TableCell className="tableCell">{`${transaction.action}: ${formatDate(transaction.time)}`}</TableCell>
                 <TableCell className="tableCell"></TableCell>
                 <TableCell className="tableCell"></TableCell>
               </TableRow>
               <TableRow>
                 <TableCell className="tableCell"><b>{transaction.action === "Dispense" ? 'Dispensed Commodity' : 'Stocked Commodity'}</b></TableCell>
-                <TableCell className="tableCell"><b>{transaction.action === "Dispense" ? 'Dispensed by' : 'Updated by'}</b></TableCell>
-                <TableCell className="tableCell"><b>Value change</b></TableCell>
+                <TableCell className="tableCell"><b>{transaction.action === "Dispense" ? 'Dispensed By' : 'Updated By'}</b></TableCell>
+                <TableCell className="tableCell"><b>Value Change</b></TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
