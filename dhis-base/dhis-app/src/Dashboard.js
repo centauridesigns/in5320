8ecafdd80b4531e3181c5426d0ba012531d4242f
centauridesigns <souhailz@ulrik.uio.no>
2023-11-15 20:33:11 +0100

    Attempt fixing Mac dates
    Added method using regex to convert dates to JS-friendly format for Mac.

--- dhis-base/dhis-app/src/Dashboard.js
+++ dhis-base/dhis-app/src/Dashboard.js
@@ -9,6 +9,8 @@ function formatDate(dateString) {
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const suffixes = ["th", "st", "nd", "rd"];
 
+  const dateString = dateString.replace(/(\d{2})\.(\d{2})\.(\d{4})/, '$2/$1/$3'); // Ensures MAC dates are correctly converted.
+
   const date = new Date(dateString);
   const day = date.getDate();
   const monthIndex = date.getMonth();
